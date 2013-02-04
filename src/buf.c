2eacafa6de5f7b7df5b5fb88461b6bfeae6d12de
Masatoshi Teruya <mah0x211@gmail.com>
2013-02-04 12:18:53 +0900

    change: buf_strnset will check string pointer and length of arguments

--- src/buf.c
+++ src/buf.c
@@ -87,7 +87,7 @@ int buf_shift( Buf_t *b, size_t from, size_t idx )
 
 int buf_strnset( Buf_t *b, const char *str, size_t len )
 {
-    if( len )
+    if( str && len )
     {
         if( buf_increase( b, len + 1 ) == BUF_OK ){
             memcpy( b->mem, str, len );
